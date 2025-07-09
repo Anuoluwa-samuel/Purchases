093b1664fc1cb71747190a951e14e3b34b800a22
Ogunyemi Ayomide <ayomidesamuel365@gmail.com>
2025-07-09 10:57:02 +0100

    feat: Use environment variables for SECRET_KEY and DEBUG

--- ERPaux/settings.py
+++ ERPaux/settings.py
@@ -22,10 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*(a#ib9dq2f5rpzn=%dgg+0kif_^%hp%c6ci=ypg(k3y8h57$b'
+SECRET_KEY = config('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
+
 
 ALLOWED_HOSTS = []
 
