1c270ab24bbc1641b0916c0b19993ef5fc81220d
Timeless <Ayomidesamuel365@gmail.com>
2025-07-12 17:39:21 +0100

    feat: Add staff view and endpoint

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("This is the Index Page")
+
+def staff(request):
+    return HttpResponse("This is the staff Page")
