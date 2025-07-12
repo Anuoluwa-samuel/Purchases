a67dfcb0f48635052daaea02817d7954d48d56a3
Timeless <Ayomidesamuel365@gmail.com>
2025-07-12 17:49:21 +0100

    Fix: Update views to render HTML templates

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse(request, '')
+    return HttpResponse(request, 'index.html')
 
 def staff(request):
-    return HttpResponse(request, '')
+    return HttpResponse(request, 'vendors.html')
