2a96821ea8a42ac76ffd08f11f8f6070a04dfef2
Timeless <Ayomidesamuel365@gmail.com>
2025-07-12 18:03:22 +0100

    Fix: Update index view to use dashboard-specific template path

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'dashboard/index.html')
 
 def vendors(request):
     return render(request, 'vendors.html')
