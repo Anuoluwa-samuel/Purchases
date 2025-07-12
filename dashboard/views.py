0c105f014b00e3d5a4b869dcb8e36f627da315eb
Timeless <Ayomidesamuel365@gmail.com>
2025-07-12 17:51:28 +0100

    Fix: Use `render` instead of `HttpResponse` and rename `staff` view to `vendors`

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse(request, 'index.html')
+    return render(request, 'index.html')
 
-def staff(request):
-    return HttpResponse(request, 'vendors.html')
+def vendors(request):
+    return render(request, 'vendors.html')
