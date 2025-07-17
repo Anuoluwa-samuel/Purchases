915813dab069b38f4a11ba70bfc411782a276f0e
Ogunyemi Ayomide <ayomidesamuel365@gmail.com>
2025-07-17 12:36:10 +0100

    feat: Add Inventory model and register in admin

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,7 @@
-from django.db import models
-
-# Create your models here.
+# Example for Inventory model
+class Inventory(models.Model):
+    item_name = models.CharField(max_length=100)
+    item_description = models.TextField(blank=True)
+    quantity_in_stock = models.IntegerField(default=0)
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+    last_updated = models.DateTimeField(auto_now=True)
