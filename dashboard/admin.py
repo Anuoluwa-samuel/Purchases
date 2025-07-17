7983ed61dfb2602034536bd14b58078668c543d5
Ogunyemi Ayomide <ayomidesamuel365@gmail.com>
2025-07-17 15:06:45 +0100

    Refactor: Remove unnecessary InventoryAdmin class

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -2,11 +2,9 @@ from django.contrib import admin
 from .models import Inventory, RequestForMaterials, RequestForQuotes, QuotationsReceived, PurchasingOrders
 
 admin.site.site_header = 'Purchase Management System Dashboard'
-admin.site.register(Inventory)
-class InventoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'quantity', 'category',)
-    list_filter = ['category',]
-    
+
+
+admin.site.register(Inventory)    
 admin.site.register(RequestForMaterials)
 admin.site.register(RequestForQuotes)
 admin.site.register(QuotationsReceived)
