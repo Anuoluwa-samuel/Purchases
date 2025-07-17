5ea9c170f11dbb53d955000ad62223c396d8bca0
Ogunyemi Ayomide <ayomidesamuel365@gmail.com>
2025-07-17 14:44:44 +0100

    feat: Add initial dashboard URLs

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path 
-from . import views 
+from django.urls import path
+from .views import (
+    add_inventory, add_request_for_materials,
+    add_request_for_quotes, add_quotations_received,
+    add_purchasing_order
+)
 
-urlpatterns= [
-    path('', views.index, name='index'),
-    path('vendors/', views.vendors, name='vendors')
-] 
+urlpatterns = [
+    path('add-inventory/', add_inventory, name='add-inventory'),
+    path('add-request-materials/', add_request_for_materials, name='add-request-materials'),
+    path('add-request-quotes/', add_request_for_quotes, name='add-request-quotes'),
+    path('add-quotation/', add_quotations_received, name='add-quotation'),
+    path('add-purchase-order/', add_purchasing_order, name='add-purchase-order'),
+]
