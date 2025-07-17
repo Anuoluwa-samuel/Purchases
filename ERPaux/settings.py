932f336e4d39fb1c1ede42acbbf5afccda53717d
Ogunyemi Ayomide <ayomidesamuel365@gmail.com>
2025-07-17 16:03:45 +0100

    Updated Changes

--- ERPaux/settings.py
+++ ERPaux/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST', default='127.0.0.1'),
-        'PORT': config('DB_PORT', default='3306'),
+        'PORT': config('DB_PORT', default='3307'),
     }
 }
 
