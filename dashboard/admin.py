e5a5f92861663518950ac290b36dc8f4abf734c0
Ogunyemi Ayomide <ayomidesamuel365@gmail.com>
2025-07-17 13:18:11 +0100

    feat: Register new models in Django admin

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import Inventory
+from .models import Inventory, RequestForMaterials, RequestForQuotes, QuotationsReceived, PurchasingOrders
 
 admin.site.register(Inventory)
+admin.site.register(RequestForMaterials)
+admin.site.register(RequestForQuotes)
+admin.site.register(QuotationsReceived)
+admin.site.register(PurchasingOrders)
