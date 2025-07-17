3b768be5fbcb54fb54cd98a481dd8de7ebaa1842
Ogunyemi Ayomide <ayomidesamuel365@gmail.com>
2025-07-17 14:05:44 +0100

    feat: Remove unused `vendors` view

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,3 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return render(request, 'dashboard/index.html')
-
-def vendors(request):
-    return render(request, 'vendors.html')
