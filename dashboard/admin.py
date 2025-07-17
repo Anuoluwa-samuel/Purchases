d45a42c425d4009f45cb73a4d727a603327be59f
Ogunyemi Ayomide <ayomidesamuel365@gmail.com>
2025-07-17 15:04:44 +0100

    feat: Add ProductAdmin and customize admin site header

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from .models import Inventory, RequestForMaterials, RequestForQuotes, QuotationsReceived, PurchasingOrders
 
+admin.site.site_header = 'Purchase Management System Dashboard'
 admin.site.register(Inventory)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'quantity', 'category',)
+    list_filter = ['category',]
+    
 admin.site.register(RequestForMaterials)
 admin.site.register(RequestForQuotes)
 admin.site.register(QuotationsReceived)
